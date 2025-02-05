800518f2b0762e932c9d7c2bdb34169df72b96f5
jackw <wolakj@mail.gvsu.edu>
2025-02-05 16:57:49 -0500

    finsihed baics of services page. added temp photos

--- frontend/buck3d/app/scans/page.tsx
+++ frontend/buck3d/app/scans/page.tsx
@@ -3,8 +3,7 @@ export default function Scans() {
     <main className="bg-stone-300 min-h-screen font-[family-name:var(--font-geist-sans)]">
       <div className="grid grid-cols-[300px_1fr] gap-4 p-4 w-full pt-[120px]">
         {/*Upload file box*/}
-        <div className="column-start-1 flex flex-col items-center text-black text-lg justify-center box border-4 border-black bg-white p-1 h-[200px] w-[300px]"
-        >
+        <div className="column-start-1 flex flex-col items-center text-black text-lg justify-center box border-4 border-black bg-white p-1 h-[200px] w-[300px]">
           Upload Your Buck Photo Here
           {/*need to add action photo upload to cloud*/}
           <form action="" className="text-sm p-3 space-y-12">
@@ -22,38 +21,27 @@ export default function Scans() {
         </div>
         {/*Antler Viewing Box*/}
         <div className="whitespace-nowrap items-start column-start-2 grid grid-rows-[40px-1fr-40px]">
-          <div className="p-4 text-2xl font-bold text-black items-start"
-          >
+          <div className="p-4 text-2xl font-bold text-black items-start">
             3D Viewer
           </div>
-          <div className="text-5xl text-black h-[900px] w-[1200px] border-4 border-black"
-          >
+          <div className="text-5xl text-black h-[900px] w-[1200px] border-4 border-black">
             3D antlers go here
           </div>
           {/*Current file and scoring text*/}
           <div className="flex flex-col text-xl text-black font-semibold p-4 items-start">
-            <div className=""
-            >
-              Current Scan: (placeholder)
-            </div>
-            <div className=""
-            >
-              Pope & Young Score: (placeholder)
-            </div>
-            <div className=""
-            >
-              Boone & Crockett Score: (placeholder)
-            </div>
+            <div className="">Current Scan: (placeholder)</div>
+            <div className="">Pope & Young Score: (placeholder)</div>
+            <div className="">Boone & Crockett Score: (placeholder)</div>
           </div>
         </div>
       </div>
       {/*Grid for previous scans*/}
-      <div className="text-black text-2xl font-bold pb-6 ml-6"
-      >
+      <div className="text-black text-2xl font-bold pb-6 ml-6">
         Previous Scans
       </div>
       <div className="grid grid-cols-5 gap-10 w-full justify-evenly ml-6 mr-6 text-center">
-        <div className="relative rounded-lg bg-yellow-950 w-32 h-32 border-4 border-black">1
+        <div className="relative rounded-lg bg-yellow-950 w-32 h-32 border-4 border-black">
+          1
           <a
             className="absolute bottom-2 right-2 rounded-full border border-solid border-transparent bg-orange-500 transition-colors flex items-center justify-center bg-foreground text-black hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm h-5 w-12 px-4"
             href=""
@@ -63,7 +51,8 @@ export default function Scans() {
             View
           </a>
         </div>
-        <div className="relative rounded-lg bg-yellow-950 w-32 h-32 border-4 border-black">2
+        <div className="relative rounded-lg bg-yellow-950 w-32 h-32 border-4 border-black">
+          2
           <a
             className="absolute bottom-2 right-2 rounded-full border border-solid border-transparent bg-orange-500 transition-colors flex items-center justify-center bg-foreground text-black hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm h-5 w-12 px-4"
             href=""
@@ -73,7 +62,8 @@ export default function Scans() {
             View
           </a>
         </div>
-        <div className="relative rounded-lg bg-yellow-950 w-32 h-32 border-4 border-black">3
+        <div className="relative rounded-lg bg-yellow-950 w-32 h-32 border-4 border-black">
+          3
           <a
             className="absolute bottom-2 right-2 rounded-full border border-solid border-transparent bg-orange-500 transition-colors flex items-center justify-center bg-foreground text-black hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm h-5 w-12 px-4"
             href=""
@@ -83,7 +73,8 @@ export default function Scans() {
             View
           </a>
         </div>
-        <div className="relative rounded-lg bg-yellow-950 w-32 h-32 border-4 border-black">4
+        <div className="relative rounded-lg bg-yellow-950 w-32 h-32 border-4 border-black">
+          4
           <a
             className="absolute bottom-2 right-2 rounded-full border border-solid border-transparent bg-orange-500 transition-colors flex items-center justify-center bg-foreground text-black hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm h-5 w-12 px-4"
             href=""
@@ -93,7 +84,8 @@ export default function Scans() {
             View
           </a>
         </div>
-        <div className="relative rounded-lg bg-yellow-950 w-32 h-32 border-4 border-black">5
+        <div className="relative rounded-lg bg-yellow-950 w-32 h-32 border-4 border-black">
+          5
           <a
             className="absolute bottom-2 right-2 rounded-full border border-solid border-transparent bg-orange-500 transition-colors flex items-center justify-center bg-foreground text-black hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm h-5 w-12 px-4"
             href=""
