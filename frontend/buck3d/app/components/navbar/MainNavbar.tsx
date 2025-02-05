8000d2df9b21fd43a4f242bae624c0a0d0c4bc45
jackw <wolakj@mail.gvsu.edu>
2025-02-04 23:52:13 -0500

    changed navbar settings. Ready for file upload, and pulling from the database for previous scan preview.

--- frontend/buck3d/app/components/navbar/MainNavbar.tsx
+++ frontend/buck3d/app/components/navbar/MainNavbar.tsx
@@ -1,10 +1,10 @@
 export default function MainNavbar() {
     return (
-        <header className="flex items-center justify-between font-sans font-bold p-5 bg-white shadow-md w-full fixed top-0 left-0">
-            <h1 className="text-black text-2xl">BuckView3D - Bring Your Buck Photos to Virtual Life</h1>
+        <header className="flex items-center justify-between font-sans font-bold p-5 bg-white shadow-md w-full h-[100px] fixed top-0 left-0">
+            <h1 className="whitespace-nowrap text-black text-2xl">BuckView3D - Bring Your Buck Photos to Virtual Life</h1>
 
             {/* 3 buttons and profile to click at top of screen */}
-            <div className="flex gap-8">
+            <div className="flex p-5 gap-8">
                 <a
                     className="rounded-full border border-solid border-transparent bg-orange-500 transition-colors flex items-center justify-center bg-foreground text-black gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
                     href="./"
--- frontend/buck3d/app/scans/page.tsx
+++ frontend/buck3d/app/scans/page.tsx
@@ -1,53 +1,107 @@
 export default function Scans() {
   return (
     <main className="bg-stone-300 min-h-screen font-[family-name:var(--font-geist-sans)]">
-      <div className="text-2xl text-black"
-      >
-        3D Viewing of Antlers
-      </div>
-      {/*Upload file box*/}
-      <div className="flex flex-col items-center text-black text-lg justify-center box border-4 border-black bg-white p-1 h-[200px] w-[300px] absolute top-40 left-6"
-      >
-        Upload Your Buck Photo Here
-        {/*need to add action photo upload to cloud*/}
-        <form action="" className="text-sm p-3 space-y-12">
-          <input
-            type="file"
-            name="buck_photo"
-            className="text-black w-full"
-            id="file-input"
-          />
-          <input
-            type="submit"
-            value="Upload Image"
-            className="text-lg text-white border-solid border-black border-2 bg-blue-600 rounded hover:bg-[#383838] dark:hover:bg-[#ccc] px-2 h-8"
-          />
-        </form>
-      </div>
-      {/*Antler Viewing Box*/}
-      <div className="grid grid-rows-[40px-600px-40px] absolute w-[700px] top-[96px] right-6">
-        <div className="row-span-1 col-span-full p-4 text-2xl text-bold text-black items-start"
+      <div className="grid grid-cols-[300px_1fr] gap-4 p-4 w-full pt-[120px]">
+        {/*Upload file box*/}
+        <div className="column-start-1 flex flex-col items-center text-black text-lg justify-center box border-4 border-black bg-white p-1 h-[200px] w-[300px]"
         >
-          3D Viewer
+          Upload Your Buck Photo Here
+          {/*need to add action photo upload to cloud*/}
+          <form action="" className="text-sm p-3 space-y-12">
+            <input
+              type="file"
+              name="buck_photo"
+              className="text-black w-full"
+            />
+            <input
+              type="submit"
+              value="Upload Image"
+              className="text-lg text-white border-solid border-black border-2 bg-blue-600 rounded hover:bg-[#383838] dark:hover:bg-[#ccc] px-2 h-8"
+            />
+          </form>
         </div>
-        <div className="row-span-2 col-span-full text-5xl text-black text-bold h-[600px] border-4 border-black"
-        >
-          3D antlers go here
+        {/*Antler Viewing Box*/}
+        <div className="whitespace-nowrap items-start column-start-2 grid grid-rows-[40px-1fr-40px]">
+          <div className="p-4 text-2xl font-bold text-black items-start"
+          >
+            3D Viewer
+          </div>
+          <div className="text-5xl text-black h-[900px] w-[1200px] border-4 border-black"
+          >
+            3D antlers go here
+          </div>
+          {/*Current file and scoring text*/}
+          <div className="flex flex-col text-xl text-black font-semibold p-4 items-start">
+            <div className=""
+            >
+              Current Scan: (placeholder)
+            </div>
+            <div className=""
+            >
+              Pope & Young Score: (placeholder)
+            </div>
+            <div className=""
+            >
+              Boone & Crockett Score: (placeholder)
+            </div>
+          </div>
         </div>
-        {/*Current file and scoring text*/}
-        <div className="flex flex-col text-xl text-black text-bold p-4 items-start">
-          <div className="flex"
+      </div>
+      {/*Grid for previous scans*/}
+      <div className="text-black text-2xl font-bold pb-6 ml-6"
+      >
+        Previous Scans
+      </div>
+      <div className="grid grid-cols-5 gap-10 w-full justify-evenly ml-6 mr-6 text-center">
+        <div className="relative rounded-lg bg-yellow-950 w-32 h-32 border-4 border-black">1
+          <a
+            className="absolute bottom-2 right-2 rounded-full border border-solid border-transparent bg-orange-500 transition-colors flex items-center justify-center bg-foreground text-black hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm h-5 w-12 px-4"
+            href=""
+            target="_self"
+            rel="noopener noreferrer"
           >
-            Current Scan: file.obj
-          </div>
-          <div className="flex"
+            View
+          </a>
+        </div>
+        <div className="relative rounded-lg bg-yellow-950 w-32 h-32 border-4 border-black">2
+          <a
+            className="absolute bottom-2 right-2 rounded-full border border-solid border-transparent bg-orange-500 transition-colors flex items-center justify-center bg-foreground text-black hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm h-5 w-12 px-4"
+            href=""
+            target="_self"
+            rel="noopener noreferrer"
           >
-            Pope & Young Score: 0
-          </div>
-          <div className="flex"
+            View
+          </a>
+        </div>
+        <div className="relative rounded-lg bg-yellow-950 w-32 h-32 border-4 border-black">3
+          <a
+            className="absolute bottom-2 right-2 rounded-full border border-solid border-transparent bg-orange-500 transition-colors flex items-center justify-center bg-foreground text-black hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm h-5 w-12 px-4"
+            href=""
+            target="_self"
+            rel="noopener noreferrer"
           >
-            Boone & Crockett Score: 0
-          </div>
+            View
+          </a>
+        </div>
+        <div className="relative rounded-lg bg-yellow-950 w-32 h-32 border-4 border-black">4
+          <a
+            className="absolute bottom-2 right-2 rounded-full border border-solid border-transparent bg-orange-500 transition-colors flex items-center justify-center bg-foreground text-black hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm h-5 w-12 px-4"
+            href=""
+            target="_self"
+            rel="noopener noreferrer"
+          >
+            View
+          </a>
+        </div>
+        <div className="relative rounded-lg bg-yellow-950 w-32 h-32 border-4 border-black">5
+          <a
+            className="absolute bottom-2 right-2 rounded-full border border-solid border-transparent bg-orange-500 transition-colors flex items-center justify-center bg-foreground text-black hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm h-5 w-12 px-4"
+            href=""
+            target="_self"
+            rel="noopener noreferrer"
+          >
+            View
+          </a>
         </div>
       </div>
     </main>
