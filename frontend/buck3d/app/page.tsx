34dd8524cdaec6fb362cade08a160ffeba87e60b
Blaze Lauer <blazelauer@gmail.com>
2025-02-25 18:40:52 -0500

    updated homepage layout

--- frontend/buck3d/app/page.tsx
+++ frontend/buck3d/app/page.tsx
@@ -13,17 +13,22 @@ export default function Home() {
       const scene = new THREE.Scene();
       const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
       const renderer = new THREE.WebGLRenderer({ alpha: true, antialias: true});
-      renderer.setSize(150, 150);
+      renderer.setSize(1500, 1000);
       renderer.setClearColor(0x000000, 0);
       antlersRef.current.appendChild(renderer.domElement);
 
       const light = new THREE.AmbientLight(0xffffff, 1);
       scene.add(light);
 
-      const geometry = new THREE.BoxGeometry(3, 3, 3);
-      const material = new THREE.MeshBasicMaterial({ color: 0x808080 });
+      const geometry = new THREE.BoxGeometry(2, 2, 2);
+      const material = new THREE.MeshBasicMaterial({ color: 0xf97316 });
       const cube = new THREE.Mesh(geometry, material);
       scene.add(cube);
+
+      const edges = new THREE.EdgesGeometry(geometry);
+      const lineMaterial = new THREE.LineBasicMaterial({ color: 0x000000 });
+      const lineSegments = new THREE.LineSegments(edges, lineMaterial);
+      scene.add(lineSegments);
 
       camera.position.z = 5;
 
@@ -31,6 +36,7 @@ export default function Home() {
       controls.enableDamping = true;
       controls.dampingFactor = 0.25;
       controls.screenSpacePanning = false;
+      controls.enableZoom = false;
 
       const animate = () => {
         requestAnimationFrame(animate);
@@ -43,26 +49,29 @@ export default function Home() {
   }, []);
 
   return (
-    <div className="bg-gray-100 min-h-screen flex flex-col items-center pt-24">
-      <div className="bg-white p-6 mt-6 w-11/12 max-w-lg shadow-lg rounded-lg text-center">
-        <div className="flex justify-center items-center gap-4 mt-4">
-          <Image src="/3Dbuck-logo.png" width={275} height={275} alt="Logo"/>
-        </div>
-          <div ref={antlersRef} className="w-11/12 max-w-lg mt-4 mx-auto flex justify-center items-center"></div>
-      </div>
-
-      <div className="bg-orange-500 text-white p-6 mt-6 w-11/12 max-w-lg rounded-lg text-center">
+    <div className="bg-white min-h-screen flex flex-row items-start justify-between pt-12">
+      <div className="bg-orange-500 text-white p-4 mt-20 ml-6 max-w-lg shadow-lg rounded-lg">
         <p className="mb-2 font-bold">1. Upload a photo of your buck</p>
         <p className="mb-2 font-bold">2. View the 3D model of your antlers</p>
         <p className="mb-2 font-bold">3. Purchase a 3D printed replica or taxidermy</p>
-        <div className="flex justify-center items-center gap-4 mt-4">
+        <div className="flex justify-center items-center gap-2 mt-4">
           <Image src="/whitetail-deer-.jpg" width={200} height={200} alt="Buck"/>
           <span className="text-2x1">→</span>
           <Image src="/3D_printed_photo.jpg" width={200} height={200} alt="Antlers"/>
         </div>
+          <p className="font-bold text-lg mt-4 text-center">Subscription Available!</p>
       </div>
 
-      <p className="text-orange-700 font-bold text-lg mt-4">Subscription Available!</p>
+      <div className="p-6 mt-6 mb-6 flex justify-center items-center w-[500px]">
+        <div ref={antlersRef} className="w-full max-w-4xl mx-auto flex justify-center items-center"></div>
+      </div>
+
+      <div className="bg-white flex justify-center" style={{ marginBottom: '12px' }}>
+        <div className="flex justify-center items-center">
+          <Image src="/logo.png" width={400} height={500} alt="Logo" style={{ objectFit: "contain", height: "500px" }}/>
+        </div>
+      </div>
+
     </div>
   );
 };
