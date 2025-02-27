32a3c07fb817b3437613f1cdcbb9e8cc3c4bf2d7
Andrewslayton <andrewsla1726@gmail.com>
2025-02-27 00:37:34 -0500

    clean up

--- frontend/buck3d/app/components/Buttons/AuthButton.tsx
+++ frontend/buck3d/app/components/Buttons/AuthButton.tsx
@@ -1,20 +1,27 @@
 "use client";
 
 import { signIn, signOut, useSession } from "next-auth/react";
-// import { useEffect, useState } from "react";
 
 export default function AuthButton() {
   const { data: session } = useSession();
 
-
   return session ? (
-    <div>
-      <p>
-        Welcome, {session.user?.name} ({session.user?.email})
-      </p>
-      <button onClick={() => signOut()}>Sign Out</button>
+    <div className="flex items-center space-x-2 text-black">
+      {session.user?.image && (
+        <img
+          src={session.user.image}
+          alt="Profile Image"
+          className="w-8 h-8 rounded-full"
+        />
+      )}
+      <span>Profile</span>
+      <button onClick={() => signOut()} className="ml-2">
+        Sign Out
+      </button>
     </div>
   ) : (
-    <button onClick={() => signIn("google")}>Sign In with Google</button>
+    <button onClick={() => signIn("google")} className="text-black">
+      Sign In with Google
+    </button>
   );
 }
--- frontend/buck3d/prisma/migrations/20250224024023_create_2dscans/migration.sql
+++
@@ -1,9 +0,0 @@
--- CreateTable
-CREATE TABLE "Scan2D" (
-    "scanid" TEXT NOT NULL,
-    "userid" TEXT NOT NULL,
-    "url" TEXT NOT NULL,
-    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
-
-    CONSTRAINT "Scan2D_pkey" PRIMARY KEY ("scanid")
-);
