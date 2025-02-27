32101a04d88ba199fe8e18c1699e8286b561d5f2
Andrewslayton <andrewsla1726@gmail.com>
2025-02-27 01:04:29 -0500

    local environment url

--- backend/main.py
+++ backend/main.py
@@ -1,5 +1,7 @@
 import uuid
 from fastapi import FastAPI
+from dotenv import load_dotenv
+
 
 app = FastAPI()
 
@@ -16,6 +18,8 @@ import os
 
 app = FastAPI()
 
+load_dotenv()
+
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["http://localhost:3000"],
@@ -24,7 +28,7 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-DATABASE_URL = "postgresql://postgres:andrew@localhost:5432/buck3d"
+DATABASE_URL = os.getenv("DATABASE_URL")
 
 conn = psycopg2.connect(DATABASE_URL, cursor_factory=RealDictCursor)
 cur = conn.cursor()
